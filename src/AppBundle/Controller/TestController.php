2d6ff39ceb6ce2836cf0758e29e5e1499599c948
lruhier <yoyo2042@hotmail.com>
2016-07-04 20:05:00 +0000

    test h2

---
+++ src/AppBundle/Controller/TestController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Debug\Exception\FlattenException;
+
+class TestController extends Controller
+{
+
+    public function indexAction(Request $request)
+    {
+    	$h2 = $request->attributes->get('h2');
+        $max = 200;
+        $response = $this->render('AppBundle:Test:test.html.twig', ['max' => $max]);
+        for ($i = 1; $i <= $max; $i++) {
+            header("Link: </images/{$i}.jpg>; rel=preload; as=image", false);
+        }
+
+        return $response;
+    }
+
+}
